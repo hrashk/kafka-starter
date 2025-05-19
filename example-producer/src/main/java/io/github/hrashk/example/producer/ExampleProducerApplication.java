26f58a4656563a73cb8bf49271e01b6904fdde5e
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2025-05-19 17:08:02 +0200

    add producer implementation

--- example-producer/src/main/java/io/github/hrashk/example/producer/ExampleProducerApplication.java
+++ example-producer/src/main/java/io/github/hrashk/example/producer/ExampleProducerApplication.java
@@ -1,13 +1,34 @@
 package io.github.hrashk.example.producer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleProducerApplication {
+public class ExampleProducerApplication implements CommandLineRunner {
+    private final KafkaProducer<String, String> producer;
+
+    @Value("${app.topic}")
+    private String topic;
+
+    public ExampleProducerApplication(KafkaProducer<String, String> producer) {
+        this.producer = producer;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleProducerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        int i = 1;
+        while (true) {
+            producer.send(new ProducerRecord<>(topic, "key-" + i, "message-" + i));
+            Thread.sleep(1000L);
+            i++;
+        }
+    }
 }
