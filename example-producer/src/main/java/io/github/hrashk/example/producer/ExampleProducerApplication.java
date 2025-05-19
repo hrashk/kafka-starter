579cd179259059a59f35c3eb8929a11bfb0d4fa5
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2025-05-19 14:29:44 +0200

    add producer example

---
+++ example-producer/src/main/java/io/github/hrashk/example/producer/ExampleProducerApplication.java
@@ -0,0 +1,13 @@
+package io.github.hrashk.example.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleProducerApplication.class, args);
+    }
+
+}
